efc6436573f092e62219540616b0fab42d29ee75
befikar <kaswanpritam@gmail.com>
2019-10-25 19:39:42 +0530

    Added necessary models to store & track web-scraped data

--- src/scraper/models.py
+++ src/scraper/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
+from django.conf import settings
 # Create your models here.
+
+
+
+class Article(models.Model):
+    source = models.CharField(max_length=30)
+    title = models.CharField(max_length=150)
+    url = models.TextField()
+    detail = models.TextField()
+    added_on = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,)
+    last_scrape = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return "{}-{}".format(self.user, self.last_scrape)
+
+
+
+
